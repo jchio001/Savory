d3143ba0d8f663303c20437dc600a1d645d47d52
Jonathan Chiou <jchio001@ucr.edu>
2018-07-22 20:56:08 -0700

    Set up Facebook login skeleton

---
+++ app/src/main/java/com/savory/login/LoginActivity.kt
@@ -0,0 +1,44 @@
+package com.savory.login
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.FrameLayout
+import android.widget.Toast
+import butterknife.BindView
+import butterknife.ButterKnife
+import com.facebook.FacebookCallback
+import com.facebook.FacebookException
+import com.facebook.login.LoginResult
+import com.savory.R
+
+class LoginActivity : AppCompatActivity() {
+
+    @BindView(R.id.facebook_button) lateinit var facebookButton : FrameLayout
+
+    val facebookClient = FacebookClient()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+        ButterKnife.bind(this)
+
+        facebookClient.bindButton(facebookButton)
+        facebookClient.registerCallback(object : FacebookCallback<LoginResult> {
+            override fun onSuccess(result: LoginResult?) {
+                Toast.makeText(this@LoginActivity, "We did it!", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onCancel() {
+            }
+
+            override fun onError(error: FacebookException?) {
+            }
+        })
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        facebookClient.onResultReceived(requestCode, resultCode, data)
+    }
+}

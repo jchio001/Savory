1d0199cf375f16669af6a36d9c3c86fb531706c1
Jonathan Chiou <jchio001@ucr.edu>
2018-08-03 21:28:57 -0700

    Added rudimentary bottom nav with fragments

--- app/src/main/java/com/savory/home/HomeActivity.java
+++ app/src/main/java/com/savory/home/HomeActivity.java
@@ -2,15 +2,66 @@ package com.savory.home;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
 
 import com.savory.R;
 
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class HomeActivity extends AppCompatActivity {
+
+    @BindView(R.id.action_home) ImageView actionHome;
+    @BindView(R.id.action_profile) ImageView actionProfile;
+
+    private FragmentManager fragmentManager;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        ButterKnife.bind(this);
+
+        fragmentManager = getSupportFragmentManager();
+        if (fragmentManager.getBackStackEntryCount() == 0) {
+            fragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, new FeedFragment())
+                .addToBackStack("FeedFragment")
+                .commit();
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (fragmentManager.getBackStackEntryCount() > 1) {
+            super.onBackPressed();
+        } else {
+            finish();
+        }
+    }
+
+    @OnClick(R.id.action_home)
+    public void onActionHome() {
+        List<Fragment> fragmentList = fragmentManager.getFragments();
+        if (!(fragmentList.get(fragmentList.size() - 1) instanceof FeedFragment)) {
+            fragmentManager.popBackStack();
+        }
+    }
+
+    @OnClick(R.id.action_profile)
+    public void onActionProfile() {
+        List<Fragment> fragmentList = fragmentManager.getFragments();
+        if (!(fragmentList.get(fragmentList.size() - 1) instanceof ProfileFragment)) {
+            fragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, new ProfileFragment())
+                .addToBackStack("ProfileFragment")
+                .commit();
+        }
     }
 }

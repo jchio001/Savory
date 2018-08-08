ed9bc099dd32ab155a84c30b1942ebbe0d2b88ac
Jonathan Chiou <jchio001@ucr.edu>
2018-08-07 20:19:28 -0700

    Added/update contracts for new endpoints and removed grey background for images

--- app/src/main/java/com/savory/api/SavoryService.java
+++ app/src/main/java/com/savory/api/SavoryService.java
@@ -1,10 +1,15 @@
 package com.savory.api;
 
 import com.savory.api.models.AccountInfo;
+import com.savory.api.models.Photo;
 import com.savory.api.models.SavoryToken;
 
+import java.util.List;
+
 import retrofit2.Call;
+import retrofit2.http.Field;
 import retrofit2.http.GET;
+import retrofit2.http.Header;
 import retrofit2.http.Query;
 
 /**
@@ -18,6 +23,29 @@ public interface SavoryService {
     @GET("/connect")
     Call<SavoryToken> connectWithSocial(@Query("token") String socialPlatformToken);
 
-    @GET("/me")
+    /**
+     * Represents a user getting their own account information
+     */
+    @GET("/account/me")
     Call<AccountInfo> getMyAccountInfo(@Query("token") String savoryToken);
+
+    /**
+     * Represents getting a page of photos for a user's feed
+     */
+    @GET("/photos")
+    Call<List<Photo>> getPhotos(@Header("Authorization") String savoryToken,
+                                @Query("last_id") Integer lastId);
+
+    /**
+     * Represents getting a page of photos for a specific account
+     */
+    @GET("/account/photos/{user_id}")
+    Call<List<Photo>> getAccountPhotos(@Field("user_id") int userId);
+
+    /**
+     * Represents an account getting a page of their own photos.
+     */
+    @GET("/account/me/photos")
+    Call<List<Photo>> getMyPhotos(@Header("Authorization") String savoryToken,
+                                  @Field("last_id") Integer lastId);
 }
